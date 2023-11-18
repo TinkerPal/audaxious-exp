050d2fea0a15bcb78e4ec78d3dc8ae8e1695549f
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2023-11-18 11:17:59 +0100

    add links & fix footer

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -1,36 +1,36 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
-import Audaxios from "./Logo";
+import Audaxios from './Logo';
 
-import { ReactComponent as Facebook } from "../assets/svg/facebook.svg";
-import { ReactComponent as Instagram } from "../assets/svg/ig.svg";
-import { ReactComponent as X } from "../assets/svg/x.svg";
-import { ReactComponent as Github } from "../assets/svg/github.svg";
-import { ReactComponent as Cmc } from "../assets/svg/cmc.svg";
-import { ReactComponent as Telegram } from "../assets/svg/tg.svg";
-import { ReactComponent as Medium } from "../assets/svg/medium.svg";
-import { ReactComponent as Discord } from "../assets/svg/discord.svg";
+import { ReactComponent as Facebook } from '../assets/svg/facebook.svg';
+import { ReactComponent as Instagram } from '../assets/svg/ig.svg';
+import { ReactComponent as X } from '../assets/svg/x.svg';
+import { ReactComponent as Github } from '../assets/svg/github.svg';
+import { ReactComponent as Cmc } from '../assets/svg/cmc.svg';
+import { ReactComponent as Telegram } from '../assets/svg/tg.svg';
+import { ReactComponent as Medium } from '../assets/svg/medium.svg';
+import { ReactComponent as Discord } from '../assets/svg/discord.svg';
 
 function Footer(props) {
   return (
-    <div className="container relative w-full text-white font-Poppins">
-      <div className="pb-7 pt-14">
-        <div className="flex flex-wrap mb-16 gap-x-24 md:gap-x-24 gap-y-8">
-          <div className="md:mr-32" style={{ maxWidth: 400 }}>
+    <div className='container relative w-full text-white font-Poppins'>
+      <div className='pb-7 pt-14'>
+        <div className='flex flex-wrap mb-16 gap-x-24 md:gap-x-24 gap-y-8'>
+          <div className='md:mr-32' style={{ maxWidth: 400 }}>
             <Audaxios />
-            <p className="text-[11px] leading-[15.63px] font-light mt-3">
+            <p className='md:text-[11px] text-[7.5px] md:leading-[15.63px] font-light md:mt-3 mt-2'>
               making access to web 3.0 simple...
             </p>
           </div>
           {GROUP_LINKS.map((group, key) => (
             <div key={key}>
-              <h6 className="mb-6 text-[20px] leading-[30px] font-medium uppercase">
+              <h6 className='mb-6 text-[20px] leading-[30px] font-medium uppercase'>
                 {group.name}
               </h6>
               {group.links.map((link, key) => (
                 <div
                   key={key}
-                  className="mb-4 text-[#71757D] text-[16px] leading-[24px] font-light"
+                  className='mb-4 text-[#71757D] text-[16px] leading-[24px] font-light'
                 >
                   <NavLink {...link} />
                 </div>
@@ -39,20 +39,42 @@ function Footer(props) {
           ))}
         </div>
 
-        <div className="border-t-[1px] border-[#71757D]">
-          <div className="flex flex-wrap items-center justify-center gap-2 pt-5 md:pt-5">
-            <p className="text-[#4F5259] md:flex-1 text-left font-medium font-Raleway text-[14px]">
+        <div className='border-t-[1px] border-[#71757D]'>
+          <div className='flex flex-wrap items-center justify-center gap-2 pt-5 md:pt-5'>
+            <p className='text-[#4F5259] md:flex-1 text-left font-medium font-Raleway text-[14px]'>
               © 2023 AudaXious. | All rights reserved
             </p>
-            <div className="flex items-center gap-4 md:ml-auto">
-              <X />
+            <div className='flex items-center gap-4 md:ml-auto'>
+              <a
+                href='https://twitter.com/AudaXious3'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <X />
+              </a>
+
               <Telegram />
               <Instagram />
               <Discord />
               <Medium />
-              <Cmc />
+
+              <a
+                href='https://coinmarketcap.com/community/profile/AudaXious/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <Cmc />
+              </a>
+
               <Facebook />
-              <Github />
+
+              <a
+                href='https://github.com/AudaXious'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <Github />
+              </a>
             </div>
           </div>
         </div>
@@ -81,27 +103,27 @@ export default Footer;
 
 const GROUP_LINKS = [
   {
-    name: "product",
+    name: 'product',
     links: [
-      { children: "features", to: "/" },
-      { children: "solution", to: "/" },
-      { children: "pricing", to: "/" },
+      { children: 'features', to: '/' },
+      { children: 'solution', to: '/' },
+      { children: 'pricing', to: '/' },
     ],
   },
   {
-    name: "resources",
+    name: 'resources',
     links: [
-      { children: "FAQ's", to: "/" },
-      { children: "documentation", to: "/" },
-      { children: "apps", to: "/" },
+      { children: "FAQ's", to: '/' },
+      { children: 'documentation', to: '/' },
+      { children: 'apps', to: '/' },
     ],
   },
   {
-    name: "company",
+    name: 'company',
     links: [
-      { children: "about us", to: "/" },
-      { children: "news", to: "/" },
-      { children: "blog", to: "/" },
+      { children: 'about us', to: '/' },
+      { children: 'news', to: '/' },
+      { children: 'blog', to: '/' },
     ],
   },
 ];
