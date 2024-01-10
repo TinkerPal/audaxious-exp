56934627374d9ddb26292a2ddac6983156ad650c
TinkerPal <tinkerpi2023@gmail.com>
2024-01-10 04:59:17 -0500

    web3

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import AppProtected from "./AppProtected";
 import AppPublic from "./AppPublic";
-<<<<<<< HEAD
 import Wagmi from "./config/wagmi/Wagmi";
-=======
->>>>>>> cb9ec42ba0f37353a630764e9f68ced04160ce41
 
 function App() {
   let isLoggedIn = false;
 
-<<<<<<< HEAD
   return <Wagmi>{isLoggedIn ? <AppProtected /> : <AppPublic />}</Wagmi>;
-=======
-  return isLoggedIn ? <AppProtected /> : <AppPublic />;
->>>>>>> cb9ec42ba0f37353a630764e9f68ced04160ce41
 }
 
 export default App;
