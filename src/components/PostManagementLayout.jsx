d642fe65dece25cb13cde4e2ec0f320aa35da795
AudaXiousWeb3 <123374158+AudaXiousWeb3@users.noreply.github.com>
2024-02-05 01:55:59 -0500

    Update PostManagementLayout.jsx

--- src/components/PostManagementLayout.jsx
+++ src/components/PostManagementLayout.jsx
@@ -43,8 +43,7 @@ const PostManagementLayout = () => {
   const [selected, setSelected] = useState(null);
 
   const authUser = useAuthUser();
-  // const screenName = authUser?.twitter?.screenName;
-  const screenName = "Hello";
+  const screenName = authUser?.twitter?.screenName;
 
   const handleSelect = (option) => {
     setSelected(option);
