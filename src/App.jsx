8baec8bee8e8cea8b85383178e811eb51d3660f0
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-02-14 03:42:22 -0800

    test the new dasboard on the root app

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import useAuthUser from "./hooks/useAuthUser";
 import AppProtected from "./AppProtected";
 import AppPublic from "./AppPublic";
 import Wagmi from "./config/wagmi/Wagmi";
+import AppProtectedNew from "./AppProtectedNew";
 
 function App() {
   let authUser = useAuthUser();
   // authUser = false;
 
-  return <Wagmi>{authUser?.token ? <AppProtected /> : <AppPublic />}</Wagmi>;
+  // return <Wagmi>{authUser?.token ? <AppProtected /> : <AppPublic />}</Wagmi>;
+  return <AppProtectedNew />;
 }
 
 export default App;
