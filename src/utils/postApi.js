0238175edc53c99e8bf9d3992463ff51b6add585
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-02-17 00:48:47 +0100

    post api utils

---
+++ src/utils/postApi.js
@@ -0,0 +1,118 @@
+export const POST = [
+  {
+    id: "p1",
+    userName: "justmylife_222",
+    time: "2",
+    coin: { bnb: "10" },
+    tasks: "4",
+    tweet: {
+      description:
+        "🚀 Hello Everyone, I am a fourth-year student of archi at the Federal University of Technology Owerri. For this semester, we are tasked with a mass housing project with eco-friendliness objectives. Please, if anyone has any resources that might be helpful, thank you!",
+      images: [],
+    },
+    repost: "125",
+    like: "1200",
+    quote: "10",
+  },
+  {
+    id: "p2",
+    userName: "justmylife_222",
+    time: "2",
+    coin: { eth: "65" },
+    tasks: "7",
+    tweet: {
+      description:
+        "🚀 Hello Everyone, I am a fourth-year student of archi at the Federal University of Technology Owerri. For this semester, we are tasked with a mass housing project with eco-friendliness objectives. Please, if anyone has any resources that might be helpful, thank you!",
+      images: [],
+    },
+    repost: "125",
+    like: "1200",
+    quote: "10",
+  },
+  {
+    id: "p3",
+    userName: "justmylife_222",
+    time: "2",
+    tweet: {
+      description:
+        "🚀 Hello Everyone, I am a fourth-year student of archi at the Federal University of Technology Owerri. For this semester, we are tasked with a mass housing project with eco-friendliness objectives. Please, if anyone has any resources that might be helpful, thank you!",
+      images: [],
+    },
+    repost: "125",
+    like: "1200",
+    quote: "10",
+    coin: { bnb: "6" },
+    tasks: "3",
+  },
+  {
+    id: "p4",
+    userName: "justmylife_222",
+    time: "2",
+    tweet: {
+      description:
+        "🚀 Hello Everyone, I am a fourth-year student of archi at the Federal University of Technology Owerri. For this semester, we are tasked with a mass housing project with eco-friendliness objectives. Please, if anyone has any resources that might be helpful, thank you!",
+      images: [],
+    },
+    repost: "125",
+    like: "1200",
+    quote: "10",
+    coin: { bnb: "4" },
+    tasks: "7",
+  },
+  {
+    id: "p5",
+    userName: "justmylife_222",
+    time: "2",
+    tweet: {
+      description: "🚀 Bitcoin is undoubtedly the Show more",
+      images: ["/tweetImages/avatar.svg", "/tweetImages/cycling.svg"],
+    },
+    repost: "125",
+    like: "1200",
+    quote: "10",
+    coin: { eth: "65" },
+    tasks: "7",
+  },
+  {
+    id: "p6",
+    userName: "justmylife_222",
+    time: "2",
+    tweet: {
+      description: "🚀 Bitcoin is undoubtedly the Show more",
+      images: ["/tweetImages/avatar.svg", "/tweetImages/cycling.svg"],
+    },
+    repost: "125",
+    like: "1200",
+    quote: "10",
+    coin: { eth: "65" },
+    tasks: "7",
+  },
+  {
+    id: "p7",
+    userName: "justmylife_222",
+    time: "2",
+    tweet: {
+      description: "🚀 Bitcoin is undoubtedly the Show more",
+      images: ["/tweetImages/avatar.svg", "/tweetImages/cycling.svg"],
+    },
+    repost: "125",
+    like: "1200",
+    quote: "10",
+    coin: { eth: "65" },
+    tasks: "7",
+  },
+  {
+    id: "p8",
+    userName: "justmylife_222",
+    time: "2",
+    tweet: {
+      description: "🚀 Bitcoin is undoubtedly the Show more",
+      images: ["/tweetImages/avatar.svg", "/tweetImages/cycling.svg"],
+    },
+    repost: "125",
+    like: "1200",
+    quote: "10",
+    coin: { eth: "65" },
+    tasks: "7",
+  },
+];
