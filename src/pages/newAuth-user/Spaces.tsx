320274d5a428938cc730c958c8e7e6a83e49037c
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-02 15:48:52 +0100

    mobile responsive and tab

--- src/pages/newAuth-user/Spaces.tsx
+++ src/pages/newAuth-user/Spaces.tsx
@@ -33,7 +33,7 @@ const Spaces = () => {
             onClick={cancelHandler}
           />
           <div className="fixed inset-0 z-[300] overflow-y-auto">
-            <div className="flex items-center justify-center mt-[2rem] text-center sm:items-center sm:p-0">
+            <div className="flex items-center py-[5rem] md:py-[0rem] justify-center mt-[2rem] text-center sm:items-center sm:p-0">
               <Dialog.Panel className="">
                 <CreateSpace />
               </Dialog.Panel>
@@ -41,7 +41,7 @@ const Spaces = () => {
           </div>
         </Dialog>
         <div>
-          <div className="flex items-end justify-between border border-[#19242D] border-r-0">
+          <div className="flex items-end justify-between gap-[1rem] md:gap-[0rem] border border-[#19242D] border-r-0 overflow-x-auto max-w-[1920px]">
             <div className="flex justify-between items-center text-[1rem] font-Poppins font-[300]">
               <div
                 onClick={() => toggleHandler(1)}
@@ -57,7 +57,7 @@ const Spaces = () => {
               <div
                 onClick={() => toggleHandler(2)}
                 className={clsx(
-                  "cursor-pointer py-[1rem] w-[100%] text-center px-[1rem]",
+                  "cursor-pointer py-[1rem] w-[100%] text-center px-[1rem] whitespace-nowrap",
                   toggle === 2
                     ? "text-[#79C4EC] bg-[#13161E] border-b-4 border-[#79C4EC]"
                     : "text-[#D3D3D3]"
@@ -78,7 +78,7 @@ const Spaces = () => {
               </div>
             </div>
             <div className="flex items-center py-[0.6rem]">
-              <div className="border-[1px] border-r-0 rounded-l-[2.5rem] border-[#2A3C46] border-opacity-[80%] py-[0.7rem] px-[1rem] cursor-pointer">
+              <div className="border-[1px] border-r-0 rounded-l-[2.5rem] border-[#2A3C46] border-opacity-[80%] py-[0.75rem] lg:py-[0.75rem] xl:py-[0.7rem] px-[1rem] cursor-pointer">
                 <SeachIcon />
               </div>
               <input
@@ -86,7 +86,7 @@ const Spaces = () => {
                 name="search"
                 id="search"
                 placeholder="search spaces.."
-                className="rounded-[2.5rem] placeholder:font-Poppins placeholder:font-[300] placeholder:text-[#536169] rounded-l-none w-[22.6rem] py-[0.5rem] border-[1px] border-[#2A3C46] border-opacity-[80%] bg-transparent outline-none px-[0.5rem]"
+                className="rounded-[2.5rem] placeholder:font-Poppins placeholder:font-[300] placeholder:text-[#536169] rounded-l-none w-[11rem] lg:w-[22.6rem] py-[0.5rem] border-[1px] border-[#2A3C46] border-opacity-[80%] bg-transparent outline-none px-[0.5rem]"
               />
             </div>
           </div>
