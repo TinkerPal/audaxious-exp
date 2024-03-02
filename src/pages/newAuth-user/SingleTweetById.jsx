e348efd6dbaaffe073eb6494ce8e66a8b9c28771
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-02 05:40:57 +0100

    updated

--- src/pages/newAuth-user/SingleTweetById.jsx
+++ src/pages/newAuth-user/SingleTweetById.jsx
@@ -1,12 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { ReactComponent as Cadabra } from "../../assets/svg/dashboardSvg/binance.svg";
 import clsx from "clsx";
 
 import { ReactComponent as ProfilePicture } from "../../assets/svg/dashboardSvg/profilePic.svg";
-import { ReactComponent as MessageIcon } from "../../assets/svg/dashboardSvg/message.svg";
-import { ReactComponent as RetweenIcon } from "../../assets/svg/dashboardSvg/retweet.svg";
-import { ReactComponent as Like } from "../../assets/svg/dashboardSvg/like.svg";
-import { ReactComponent as Friends } from "../../assets/svg/dashboardSvg/friendsPlus.svg";
 import { ReactComponent as Clock } from "../../assets/svg/dashboardSvg/clock.svg";
 import { ReactComponent as Bnb } from "../../assets/svg/dashboardSvg/bnb.svg";
 import { ReactComponent as Eth } from "../../assets/svg/dashboardSvg/eth.svg";
@@ -14,10 +9,6 @@ import { ReactComponent as Tweet } from "../../assets/svg/dashboardSvg/tweeter.s
 import { ReactComponent as Infinity } from "../../assets/svg/dashboardSvg/infinity.svg";
 import { ReactComponent as Retweet } from "../../assets/svg/dashboardSvg/secondRetweet.svg";
 import { ReactComponent as Love } from "../../assets/svg/dashboardSvg/love.svg";
-import { ReactComponent as Setting } from "../../assets/svg/dashboardSvg/setting.svg";
-import { ReactComponent as Bird } from "../../assets/svg/dashboardSvg/bird.svg";
-import { ReactComponent as Btw } from "../../assets/svg/dashboardSvg/btw.svg";
-import { ReactComponent as Fam } from "../../assets/svg/dashboardSvg/famnfrd.svg";
 import { ReactComponent as FlexLine } from "../../assets/svg/dashboardSvg/flexLine.svg";
 import { ReactComponent as Cancel } from "../../assets/svg/dashboardSvg/cancel.svg";
 import { ReactComponent as Next } from "../../assets/svg/dashboardSvg/next.svg";
@@ -90,7 +81,7 @@ const SingleTweetById = ({ onCancel, tweetId, setSelectedPostId }) => {
           </div>
           <div>
             <button className="px-[1rem] py-[0.5rem] font-Poppins text-[1rem] text-[#060B12] font-[400] bg-[#79C4EC] rounded-sm">
-              Join communityssss
+              Join space
             </button>
           </div>
         </header>
@@ -219,28 +210,6 @@ const SingleTweetById = ({ onCancel, tweetId, setSelectedPostId }) => {
                             </span>
                           </p>
                         </div>
-                        {/* <div className="flex items-baseline mb-[1rem]">
-                          <div className="flex gap-1">
-                            <div className="flex">
-                              <span>
-                                <MessageIcon />
-                              </span>
-                              <span>{post.quote}</span>
-                            </div>
-                            <div className="flex gap-[0.4rem]">
-                              <span>
-                                <RetweenIcon />
-                              </span>
-                              <span>{post.repost}</span>
-                            </div>
-                            <div className="flex gap-[0.4rem]">
-                              <span>
-                                <Like />
-                              </span>
-                              <span>{post.like}</span>
-                            </div>
-                          </div>
-                        </div> */}
                       </div>
                     </div>
                   </div>
@@ -523,226 +492,6 @@ const SingleTweetById = ({ onCancel, tweetId, setSelectedPostId }) => {
           </main>
         </div>
       </div>
-      {/* second section */}
-      {/* <div className="container flex mt-[5rem] mb-[1.5rem] items-center justify-between">
-        <span>
-          <Cadabra style={{ height: "40px", width: "40px" }} />
-        </span>
-        <p className="whitespace-nowrap font-Poppins text-[0.7rem] lg:text-[1.25rem] normal-case font-[300] text-[#A5A5A5]">
-          See more posts from Theblockchain_experts
-        </p>
-        <div className="w-[40%] h-[1px] bg-neutral-600"></div>
-        <span className="whitespace-nowrap text-[#79C4EC] font-Poppins text-[1.25rem] cursor-pointer">
-          View profile
-        </span>
-      </div> */}
-      {/* MORE ABOUT USER POST */}
-      {/* <div className="container">
-        <div className="grid grid-cols-2 xl:grid-cols-3 gap-y-[1.25rem] gap-x-[2rem] pt-[1rem] pl-[0rem]">
-          {POST.slice(0, 3).map((post) => (
-            <div
-              key={post.id}
-              className="border-[#314048] border-[0.5px] rounded-[20px] bg-heroCustom bg-no-repeat bg-cover"
-            >
-              <div className="flex justify-between mx-[0.81rem] mt-[0.9rem]">
-                <div className="flex items-center gap-[0.75rem]">
-                  <button className="bg-[#152A39] flex items-center gap-1 border-[1px] border-[#5a8686] px-[9px] py-[6px] font-Poppins text-[0.6rem] font-[300] text-[#87cece] rounded-[26px]">
-                    <span>
-                      <Clock />
-                    </span>
-                    <span>Tasks | {post.tasks}/10</span>
-                  </button>
-                  <button
-                    className={clsx(
-                      "flex items-center gap-1 border-[1px] px-[9px] py-[6px] font-Poppins text-[0.6rem] font-[300] text-[#C556E1] rounded-[26px]",
-                      post.coin.eth
-                        ? "bg-[#1F2030] text-[#C556E1] border-[#C556E1]"
-                        : "bg-[#2C2C2C] text-[#E1D356] border-[#708026]"
-                    )}
-                  >
-                    <span>
-                      engage to earn |{" "}
-                      {post.coin.eth
-                        ? `${post.coin.eth} ETH`
-                        : `${post.coin.bnb} BNB`}
-                    </span>
-                    <span>{post.coin.eth ? <Eth /> : <Bnb />}</span>
-                  </button>
-                </div>
-              </div>
-              <div className="h-[1px] bg-[#2A3C46] my-[1rem]"></div>
-              <div className="relative pb-[13px] px-[1rem]">
-                <div className="text-neutral-400 flex flex-col gap-[13px]">
-                  <div className="flex items-center gap-3">
-                    <div>
-                      <ProfilePicture />
-                    </div>
-
-                    <span>@{post.userName}</span>
-                    <span className="text-[#929192] font-[500] text-[0.67rem]">
-                      {"Dec 13, 7:44 PM"}
-                    </span>
-                  </div>
-                  <div className="flex flex-col gap-[1rem]">
-                    <div className="w-[100%] flex flex-col gap-[1rem]">
-                      <p className="text-[0.95rem]">
-                        {post.tweet?.description}
-                      </p>
-                      {post && post.tweet && post.tweet.images.length > 0 && (
-                        <div className="flex gap-[8px] mb-[30px]">
-                          {post.tweet.images.map((image) => (
-                            <div
-                              key={image}
-                              className="h-[4rem] w-[10rem] bg-white rounded-[8px] object-cover"
-                            >
-                              <img
-                                src={image}
-                                width={"400"}
-                                height={"400"}
-                                alt=""
-                              />
-                            </div>
-                          ))}
-                        </div>
-                      )}
-                    </div>
-                    <div className="flex items-baseline">
-                      <div className="flex justify-between gap-1">
-                        <div className="flex gap-[0.4rem]">
-                          <span>
-                            <MessageIcon />
-                          </span>
-                          <span>{post.quote}</span>
-                        </div>
-                        <div className="flex gap-[0.4rem]">
-                          <span>
-                            <RetweenIcon />
-                          </span>
-                          <span>{post.repost}</span>
-                        </div>
-                        <div className="flex gap-[0.4rem]">
-                          <span>
-                            <Like />
-                          </span>
-                          <span>{post.like}</span>
-                        </div>
-                        <div className="">
-                          <span>
-                            <Friends />
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div> */}
-      {/* <div className="w-100% h-[1px] bg-[#2A3C46] my-[3.25rem]"></div> */}
-
-      {/* <article className="mb-[5rem] container">
-        <div className="grid grid-cols-2 xl:grid-cols-3 gap-y-[1.25rem] gap-x-[2rem] pt-[1rem] pl-[0rem]">
-          {POST.map((post) => (
-            <div
-              key={post.id}
-              className="border-[#314048] border-[0.5px] rounded-[20px] bg-heroCustom bg-no-repeat bg-cover"
-            >
-              <div className="flex justify-between mx-[0.81rem] mt-[0.9rem]">
-                <div className="flex items-center gap-[0.75rem]">
-                  <button className="bg-[#152A39] flex items-center gap-1 border-[1px] border-[#5a8686] px-[9px] py-[6px] font-Poppins text-[0.6rem] font-[300] text-[#87cece] rounded-[26px]">
-                    <span>
-                      <Clock />
-                    </span>
-                    <span>Tasks | {post.tasks}/10</span>
-                  </button>
-                  <button
-                    className={clsx(
-                      "flex items-center gap-1 border-[1px] px-[9px] py-[6px] font-Poppins text-[0.6rem] font-[300] text-[#C556E1] rounded-[26px]",
-                      post.coin.eth
-                        ? "bg-[#1F2030] text-[#C556E1] border-[#C556E1]"
-                        : "bg-[#2C2C2C] text-[#E1D356] border-[#708026]"
-                    )}
-                  >
-                    <span>
-                      engage to earn |{" "}
-                      {post.coin.eth
-                        ? `${post.coin.eth} ETH`
-                        : `${post.coin.bnb} BNB`}
-                    </span>
-                    <span>{post.coin.eth ? <Eth /> : <Bnb />}</span>
-                  </button>
-                </div>
-              </div>
-              <div className="h-[1px] bg-[#2A3C46] my-[1rem]"></div>
-              <div className="relative pb-[13px] px-[1rem]">
-                <div className="text-neutral-400 flex flex-col gap-[13px]">
-                  <div className="flex items-center gap-3">
-                    <div>
-                      <ProfilePicture />
-                    </div>
-
-                    <span>@{post.userName}</span>
-                    <span className="text-[#929192] font-[500] text-[0.67rem]">
-                      {"Dec 13, 7:44 PM"}
-                    </span>
-                  </div>
-                  <div className="flex flex-col gap-[1rem]">
-                    <div className="w-[100%] flex flex-col gap-[1rem]">
-                      <p className="text-[0.95rem]">
-                        {post.tweet?.description}
-                      </p>
-                      {post && post.tweet && post.tweet.images.length > 0 && (
-                        <div className="flex gap-[8px] mb-[30px]">
-                          {post.tweet.images.map((image) => (
-                            <div key={image} className="rounded-[8px]">
-                              <img
-                                src={image}
-                                width={"400"}
-                                height={"400"}
-                                className="w-[100%] h-[8rem] object-cover rounded-[8px]"
-                                alt=""
-                              />
-                            </div>
-                          ))}
-                        </div>
-                      )}
-                    </div>
-                    <div className="flex items-baseline">
-                      <div className="flex justify-between gap-1">
-                        <div className="flex gap-[0.4rem]">
-                          <span>
-                            <MessageIcon />
-                          </span>
-                          <span>{post.quote}</span>
-                        </div>
-                        <div className="flex gap-[0.4rem]">
-                          <span>
-                            <RetweenIcon />
-                          </span>
-                          <span>{post.repost}</span>
-                        </div>
-                        <div className="flex gap-[0.4rem]">
-                          <span>
-                            <Like />
-                          </span>
-                          <span>{post.like}</span>
-                        </div>
-                        <div className="">
-                          <span>
-                            <Friends />
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </article> */}
     </section>
   );
 };
