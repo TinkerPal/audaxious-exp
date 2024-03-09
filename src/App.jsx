b65edb1c74d0e4c11119b82b4ed978f4760d7c79
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-09 12:28:57 +0100

    useEffect to check if a user is logged in or not

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,25 @@ import AppProtected from "./AppProtected";
 import AppPublic from "./AppPublic";
 import Wagmi from "./config/wagmi/Wagmi";
 import AppProtectedNew from "./AppProtectedNew";
+import { useEffect } from "react";
+import { getToken } from "./utils/accesstoken";
+import { useDispatch, useSelector } from "react-redux";
+import { authAction } from "./store/store";
 
 function App() {
   // let authUser = useAuthUser();
   // authUser = false;
+  const token = getToken();
+  // const isAuthenticated = useSelector((state) => state.isLogedIn);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!token) {
+      dispatch(authAction.logout());
+    } else {
+      dispatch(authAction.loggin());
+    }
+  }, [token, dispatch]);
 
   // return <Wagmi>{authUser?.token ? <AppProtected /> : <AppPublic />}</Wagmi>;
   return <AppProtectedNew />;
