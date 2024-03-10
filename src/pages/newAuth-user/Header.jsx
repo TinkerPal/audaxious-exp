c1beb069198a06a7346a337a9e1cad6a6cf57b31
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-10 19:59:07 +0100

    add menulist

--- src/pages/newAuth-user/Header.jsx
+++ src/pages/newAuth-user/Header.jsx
@@ -4,13 +4,20 @@ import { ReactComponent as Search } from "../../assets/svg/dashboardSvg/search.s
 import { ReactComponent as Bell } from "../../assets/svg/dashboardSvg/bell.svg";
 import { ReactComponent as Line } from "../../assets/svg/dashboardSvg/linebtw.svg";
 import { ReactComponent as LogoMd } from "../../assets/svg/dashboardSvg/audaxiousmd.svg";
-import { Link, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import { ReactComponent as DashBoardIcon } from "../../assets/svg/dashboardSvg/dasboardIcon.svg";
 import { ReactComponent as CommunityIcon } from "../../assets/svg/dashboardSvg/community.svg";
+import { ReactComponent as SettingsIcon } from "../../assets/svg/dashboardSvg/settings.svg";
+import { ReactComponent as ProfileIcon } from "../../assets/svg/dashboardSvg/profile.svg";
+import { ReactComponent as HelpIcon } from "../../assets/svg/dashboardSvg/help.svg";
+import { ReactComponent as LogoutIcon } from "../../assets/svg/dashboardSvg/logout.svg";
+
 import { useDispatch, useSelector } from "react-redux";
 import { authAction } from "../../store/store";
+import { useState } from "react";
 const Header = () => {
-  const token = useSelector((state) => state.isLogedIn);
+  const [showMenu, setShowMenu] = useState(false);
+  const isAuthenticated = useSelector((state) => state.isLogedIn);
 
   let title = "Dashboard";
   let specificIcon = <DashBoardIcon style={{ fill: "#FFF" }} />;
@@ -18,16 +25,18 @@ const Header = () => {
   const path = location.pathname;
   const dispatch = useDispatch();
 
+  const toggleMenuHandler = () => {
+    setShowMenu((prev) => !prev);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("loggedin");
+    dispatch(authAction.logout());
+  };
+
   const openLoginModal = () => {
-    // onOpen(bool);
     dispatch(authAction.onOpen());
   };
-  // console.log(token);
-  // useEffect(() => {
-  //   const newToken = getToken();
-  //   console.log("New token:", newToken);
-  //   setToken(newToken);
-  // }, [token]);
 
   let butons = (
     <button
@@ -38,9 +47,9 @@ const Header = () => {
     </button>
   );
 
-  if (token) {
+  if (isAuthenticated) {
     butons = (
-      <div>
+      <div onClick={toggleMenuHandler}>
         <div className="w-[2rem] h-[2rem] px-[0.4rem] py-[0.4rem] bg-[#EBBEF3] rounded-full flex items-center justify-center">
           <p className="text-[1.25rem] font-Poppins font-[600] text-neutral-950">
             H
@@ -64,69 +73,96 @@ const Header = () => {
     specificIcon = <CommunityIcon style={{ fill: "#FFF" }} />;
   }
   return (
-    <div className="max-w-[1920px] fixed top-0 left-0 right-0 z-50">
-      <div className="text-neutral-100 flex items-center justify-between relative py-[1.38rem] px-[1.2rem] md:px-[4.34rem] bg-[#060B12] border-b border-b-[#18232C]">
-        <div className="flex items-center gap-[0.5rem] md:gap-[2rem] xl:gap-[8.6rem]">
-          <div className="hidden xl:block">
-            <Link to={"/"}>
-              <Logo />
-            </Link>
+    <div className="relative max-w-[1920px]">
+      <div className="fixed top-0 left-0 right-0 z-50">
+        <div className="text-neutral-100 flex items-center justify-between relative py-[1.38rem] px-[1.2rem] md:px-[4.34rem] bg-[#060B12] border-b border-b-[#18232C]">
+          <div className="flex items-center gap-[0.5rem] md:gap-[2rem] xl:gap-[8.6rem]">
+            <div className="hidden xl:block">
+              <Link to={"/"}>
+                <Logo />
+              </Link>
+            </div>
+            <div className="block xl:hidden">
+              <Link to={"/"}>
+                <LogoMd />
+              </Link>
+            </div>
+            <div
+              className="text-[0.8rem] md:text-[1.09rem] font-Poppins font-[300] text-[#cccbcb] flex gap-4 items-center"
+              style={{ fontStyle: "normal" }}
+            >
+              <span className="">{specificIcon}</span>{" "}
+              <span className="whitespace-nowrap">{title}</span>
+            </div>
           </div>
-          <div className="block xl:hidden">
-            <Link to={"/"}>
-              <LogoMd />
-            </Link>
-          </div>
-          <div
-            className="text-[0.8rem] md:text-[1.09rem] font-Poppins font-[300] text-[#cccbcb] flex gap-4 items-center"
-            style={{ fontStyle: "normal" }}
-          >
-            <span className="">{specificIcon}</span>{" "}
-            <span className="whitespace-nowrap">{title}</span>
-          </div>
-        </div>
 
-        <div className="flex items-center gap-[0.3rem] md:gap-[1rem]">
-          <div>
-            <Bell />
+          <div className="flex items-center gap-[0.3rem] md:gap-[1rem]">
+            <div>
+              <Bell />
+            </div>
+            <div>
+              <Line />
+            </div>
+            {butons}
           </div>
-          <div>
-            <Line />
-          </div>
-          {butons}
         </div>
+        {showMenu && isAuthenticated && (
+          <div className="absolute mt-2 right-5 md:right-10 bg-[#060B12]">
+            <div
+              className="text-[#fff] w-[10rem] border-[#2A3C46] border border-opacity-[80%] bg-ElipseBg bg-no-repeat bg-cover rounded-md"
+              style={{
+                boxShadow:
+                  "0 4px 6px -1px rgba(0, 0, 0, 0.7), 0 2px 4px -1px rgba(0, 0, 0, 0.7)",
+              }}
+            >
+              <div className="flex flex-col gap-[0.5rem]">
+                <NavLink className="font-Poppins text-[1rem] font-[300] text-[#818282] flex py-[0.48rem] px-[1rem] gap-[1rem] rounded-md hover:bg-[#2C2D30] hover:border-t-[1.5px] hover:border-[#383B42]">
+                  <span>
+                    <ProfileIcon />
+                  </span>
+                  <span>Profile</span>
+                </NavLink>
+                <NavLink className="font-Poppins text-[1rem] font-[300] text-[#818282] flex py-[0.48rem] px-[1rem] gap-[1rem] rounded-md hover:bg-[#2C2D30] hover:border-t-[1.5px] hover:border-[#383B42]">
+                  <span>
+                    <SettingsIcon />
+                  </span>
+                  <span>Settings</span>
+                </NavLink>
+                <NavLink className="font-Poppins text-[1rem] font-[300] text-[#818282] flex py-[0.48rem] px-[1rem] gap-[1rem] rounded-md hover:bg-[#2C2D30] hover:border-t-[1.5px] hover:border-[#383B42]">
+                  <span>
+                    <HelpIcon />
+                  </span>
+                  <span>Help</span>
+                </NavLink>
+                {isAuthenticated && (
+                  <button
+                    onClick={logoutHandler}
+                    className="font-Poppins text-[1rem] font-[300] text-[#818282] flex py-[0.48rem] px-[1rem] gap-[1rem] rounded-md hover:bg-[#2C2D30] hover:border-t-[1.5px] hover:border-[#383B42] whitespace-nowrap"
+                  >
+                    <span>
+                      <LogoutIcon />
+                    </span>
+                    <span>Logout</span>
+                  </button>
+                )}
+                {!isAuthenticated && (
+                  <button
+                    onClick={openLoginModal}
+                    className="font-Poppins text-[1rem] font-[300] text-[#818282] flex py-[0.48rem] px-[1rem] gap-[1rem] rounded-md hover:bg-[#2C2D30] hover:border-t-[1.5px] hover:border-[#383B42] whitespace-nowrap"
+                  >
+                    <span>
+                      <LogoutIcon />
+                    </span>
+                    <span>Login</span>
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
 export default Header;
-
-<div className="max-w-[1920px]">
-  <div className="text-neutral-100 flex items-center justify-between relative py-[1.38rem] px-[4.34rem] bg-[#060B12] border-b border-b-[#18232C]">
-    <div className="flex items-center gap-[5.38rem]">
-      <div>
-        <Logo />
-      </div>
-      <div
-        className="text-[1.09rem] font-Poppins font-[300] text-[#cccbcb] flex gap-4 items-center"
-        style={{ fontStyle: "normal" }}
-      >
-        <span>specificIcon</span> <span>Engage portal</span>
-      </div>
-    </div>
-    <div className="">
-      <div>
-        <Search />
-      </div>
-      <div>
-        <Bell />
-      </div>
-      <div>
-        <div className="px-[0.19rem] py-[0.7rem] w-[2.5rem] h-[2.5rem] bg-[#EBBEF3] rounded-full">
-          <p className="text-">H</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>;
