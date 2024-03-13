2132765b868040bae18ddf4d56ec236634d531c9
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-13 18:16:39 +0100

    logout modal with integrating global state redux mangagement for logout and clearing local storage

--- src/pages/newAuth-user/Layout.jsx
+++ src/pages/newAuth-user/Layout.jsx
@@ -5,20 +5,35 @@ import { Dialog } from "@headlessui/react";
 import Authentication from "./Authentication";
 import { useDispatch, useSelector } from "react-redux";
 import { authAction } from "../../store/store";
+import VerifyTweeterModal from "../../components/socialmedia/VerifyTweetModal";
 
 const DashboardLayout = () => {
-  // const [open, setOpen] = useState(false);
+  const openLogout = useSelector((state) => state.logoutModal);
   const open = useSelector((state) => state.isOpen);
   const dispatch = useDispatch();
   const cancelHandler = () => {
     dispatch(authAction.onclose());
   };
 
+  const closeModalHandler = () => {
+    dispatch(authAction.logoutModalMethod(false));
+  };
+
+  const openLogoutModal = () => {
+    dispatch(authAction.logoutModalMethod(true));
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("audaxiousAccessToken");
+    dispatch(authAction.logout());
+    dispatch(authAction.logoutModalMethod(false));
+  };
+
   return (
     <div className="">
       <Header />
       <main className="flex my-[1.25rem] mt-[7rem] ml-[3rem] md:ml-[6rem] lg:ml-[4rem] xl:ml-[15rem]">
-        <Sidebar />
+        <Sidebar onOpen={openLogoutModal} />
         <div className="container">
           <div className="container">
             <Dialog
@@ -41,6 +56,41 @@ const DashboardLayout = () => {
               </div>
             </Dialog>
           </div>
+          <VerifyTweeterModal onClose={closeModalHandler} open={openLogout}>
+            <section className="bg-[#060B12] py-[2.5rem] rounded-md max-w-[1300px] px-[1rem]">
+              <div className="container">
+                <h2 className="text-[#dfdfdf] font-Poppins font-[600] text-[1.25rem] md:text-[2.5rem]">
+                  Logout Confirmation
+                </h2>
+                <p className="my-[0.5rem] text-[#dfdfdf] font-Poppins font-[300] text-[0.96rem] md:text-[1.3rem]">
+                  Are you sure you want to log out?
+                </p>
+                {/* <p className="text-[#A5A5A5] text-center text-[0.875rem] md:text-[1.2rem] font-Poppins leading-[140%] font-[300]">
+                  In order to continue, you need to verify your twitter account
+                </p> */}
+                <div className="flex flex-col md:flex-row items-center justify-between md:gap-[1.5rem]">
+                  <button
+                    onClick={logoutHandler}
+                    className="bg-[#E8E8E8] flex items-center justify-center rounded-[8px] border-[1.5px] border-[#4C5656] border-opacity-[10%] p-3.5 mt-8 w-[15rem] md:w-[17rem]"
+                    // mt-8 mx-auto whitespace-nowrap lg:w-[17rem]
+                  >
+                    <span className="text-[#060B12] font-Poppins font-[600]">
+                      Logout
+                    </span>
+                  </button>
+                  <button
+                    onClick={closeModalHandler}
+                    className="bg-[#E8E8E8] flex items-center justify-center rounded-[8px] border-[1.5px] border-[#4C5656] border-opacity-[10%] p-3.5 mt-8 w-[15rem] md:w-[17rem]"
+                    // mt-8 mx-auto whitespace-nowrap lg:w-[17rem]
+                  >
+                    <span className="text-[#060B12] font-Poppins font-[600]">
+                      Cancel
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </section>
+          </VerifyTweeterModal>
           <Outlet />
         </div>
       </main>
