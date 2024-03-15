275a11da701fba4a0a6de30ea660c15ab66aac5d
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-15 08:10:14 +0100

    authorization slice

---
+++ src/store/authorizationSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  isLogedIn: false,
+  isOpen: false,
+  logoutModal: false,
+  email: "",
+  verifyTweet: false,
+  loading: null,
+};
+
+const authenticationSlice = createSlice({
+  name: "authentication",
+  initialState,
+  reducers: {
+    setLoading(state, actions) {
+      state.loading = actions.payload;
+    },
+    verifyTweeterAccount(state, actions) {
+      state.verifyTweet = actions.payload;
+    },
+    setEmail(state, actions) {
+      state.email = actions.payload;
+    },
+    loggin(state) {
+      state.isLogedIn = true;
+    },
+    logoutModalMethod(state, actions) {
+      state.logoutModal = actions.payload;
+    },
+    logout(state) {
+      state.isLogedIn = false;
+    },
+    onOpen(state) {
+      state.isOpen = true;
+    },
+    onclose(state) {
+      state.isOpen = false;
+    },
+  },
+});
+
+export const authAction = authenticationSlice.actions;
+
+export default authenticationSlice;
