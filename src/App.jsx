ae149a87020b2405d0c335f3e055df3eff4f7bcf
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-15 12:37:22 +0100

    getting userDetails if user is authenticated at the start of the app

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,16 @@ import { useEffect } from "react";
 import { getToken } from "./utils/accesstoken";
 import { useDispatch, useSelector } from "react-redux";
 import { authAction } from "./store/authorizationSlice";
+import { getTwitterUserName } from "./store/authActions";
 
 function App() {
   // let authUser = useAuthUser();
   // authUser = false;
   const token = getToken();
   // const isAuthenticated = useSelector((state) => state.isLogedIn);
+  const isAuthenticated = useSelector(
+    (state) => state.authentication.isLogedIn
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -23,6 +27,22 @@ function App() {
       dispatch(authAction.loggin());
     }
   }, [token, dispatch]);
+
+  const getTwetterVerifiedUserName = async () => {
+    try {
+      const result = await dispatch(getTwitterUserName());
+      dispatch(authAction.setUserName(result.data.username));
+      dispatch(authAction.verifyTweeterAccount(result.data.twitterUsername));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      getTwetterVerifiedUserName();
+    }
+  }, [isAuthenticated]);
 
   // return <Wagmi>{authUser?.token ? <AppProtected /> : <AppPublic />}</Wagmi>;
   return <AppProtectedNew />;
