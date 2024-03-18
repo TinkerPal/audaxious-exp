ab4756b5adcfd5d5f95c3f84f730a50e3b881803
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-18 03:38:25 +0100

    cleanup and updated

--- src/pages/newAuth-user/engagePortal/EngagePortal.jsx
+++ src/pages/newAuth-user/engagePortal/EngagePortal.jsx
@@ -84,33 +84,6 @@ const EngagePortals = () => {
       // ref={scrollRef}
     >
       <div>
-        {/* <div className="container">
-          <Dialog
-            as="div"
-            className={`relative z-[800]`}
-            open={open}
-            onClose={() => {}}
-            ref={overlayRef}
-          >
-            <div
-              className="fixed inset-0 bg-black bg-opacity-75"
-              onClick={cancelHandler}
-            />
-            <div className="fixed inset-0 z-[300] overflow-y-auto">
-              <div className="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0">
-                <Dialog.Panel className="">
-                  <SingleTweetById
-                    tweetId={singleTweet}
-                    onCancel={cancelHandler}
-                    setSelectedPostId={setSelectedPostId}
-                    onLoadTweet={loadTweetByIdHandler}
-                    selectedPostId={selectedPostId}
-                  />
-                </Dialog.Panel>
-              </div>
-            </div>
-          </Dialog>
-        </div> */}
         <div className="bg-heroCustom bg-no-repeat bg-cover py-[1rem] px-[1rem] rounded-md border-[#314048] flex justify-between border-[0.5px] relative">
           <div className="absolute top-0 left-0 z-5 hidden md:block">
             <Brick1 />
