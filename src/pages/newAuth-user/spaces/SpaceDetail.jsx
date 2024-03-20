35554078c27a63b73d46e7b042aa544fcd7e36af
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-20 01:12:05 +0100

    integrate the api for space detail and consume the data available

--- src/pages/newAuth-user/spaces/SpaceDetail.jsx
+++ src/pages/newAuth-user/spaces/SpaceDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ReactComponent as Group } from "../../../assets/svg/dashboardSvg/group.svg";
 import { ReactComponent as World } from "../../../assets/svg/dashboardSvg/world.svg";
 import { ReactComponent as Retweets } from "../../../assets/svg/dashboardSvg/retweets.svg";
@@ -15,22 +15,54 @@ import Twitter from "../../../components/socialmedia/Twitter";
 import { TOPEARNERS } from "../../../utils/postApi";
 import { useDispatch, useSelector } from "react-redux";
 import { authAction } from "../../../store/authorizationSlice";
+import { useParams } from "react-router-dom";
+import { getSpaceById, joinSpace } from "../../../store/spaceActions";
+import Loading from "../../Homes/Loading";
+import { spaceActions } from "../../../store/spaceSlice";
+import { toast } from "react-toastify";
 
 const SpaceDetail = () => {
   const [toggle, setToggle] = useState(1);
+  const [spaceDetail, setSpaceDetail] = useState({});
   //   const [selectedPostId, setSelectedPostId] = useState(null);
   const dispatch = useDispatch();
   // const token = getToken();
+  const params = useParams();
+  const spaceId = params.spaceId;
   const isAuthenticated = useSelector(
     (state) => state.authentication.isLogedIn
   );
 
-  const joinSpaceHandler = () => {
+  const loading = useSelector((state) => state.space.loading);
+
+  useEffect(() => {
+    const getSpaces = async () => {
+      try {
+        const result = await dispatch(getSpaceById(spaceId));
+        setSpaceDetail(result.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getSpaces();
+  }, [dispatch, spaceId]);
+
+  const joinSpaceHandler = async () => {
     if (!isAuthenticated) {
       dispatch(authAction.onOpen());
       return;
     }
-    dispatch(authAction.loggin());
+    dispatch(spaceActions.setLoading(true));
+    try {
+      const result = await dispatch(joinSpace(spaceId));
+      dispatch(spaceActions.setLoading(false));
+      toast.success(result.message);
+
+      console.log("JOINSPACE", result);
+    } catch (error) {
+      dispatch(spaceActions.setLoading(false));
+      toast.error(error.response.data.error);
+    }
   };
   const toggleHandler = (id) => {
     setToggle(id);
@@ -40,22 +72,37 @@ const SpaceDetail = () => {
   //     setSelectedPostId(id);
   //     setSingleTweet(id);
   //   };
+
+  if (!spaceDetail) {
+    return <Loading />;
+  }
   return (
     <div className="text-[#FFF] p-[0rem] mt-[-2rem] border-[#2A3C46] md:border-l border-opacity-[80%] ml-[0.7rem] md:ml-[2rem] xl:ml-[0.7rem]">
-      <div className="h-[3.5rem] md:h-[8rem] lg:h-[8.5rem] xl:h-[10.5rem] mr-[-1.9rem]">
-        <img src="/tweetImages/header.svg" alt="" className="w-[100%]" />
+      <div className="h-[3.5rem] md:h-[8rem] lg:h-[8.5rem] xl:max-h-[10.5rem] mr-[-0.8rem] lg:mr-[-1.9rem]">
+        <img
+          src="/tweetImages/header.svg"
+          alt=""
+          className="w-[100%] h-[100%] object-cover"
+        />
       </div>
 
       <div className="md:container md:ml-[0rem]">
         <div className="flex flex-wrap md:flex-nowrap gap-[1rem] md:gap-[3rem] lg:gap-[17rem] items-end mt-[-2rem] md:mt-[-3.5rem]">
           <div className="ml-[1rem] md:ml-[0rem]">
-            <img
-              src="/tweetImages/audaxious.svg"
-              width="100"
-              height="100"
-              alt=""
-              className="xl:w-[10rem] w-[5rem] md:w-[9rem] xl:h-[10rem] md:h-[9rem] h-[5rem] object-cover rounded-full border border-[#2A3C46] border-opacity-[80%]"
-            />
+            {!spaceDetail.src && (
+              <div className="flex bg-slate-300 text-[3rem] font-[500] items-center justify-center xl:w-[10rem] w-[5rem] md:w-[9rem] xl:h-[10rem] md:h-[9rem] h-[5rem] rounded-full border text-[#2A3C46] border-[#2A3C46] border-opacity-[80%]">
+                {spaceDetail.title && spaceDetail.title.slice(0, 1)}
+              </div>
+            )}
+            {spaceDetail.src && (
+              <img
+                src="/tweetImages/audaxious.svg"
+                width="100"
+                height="100"
+                alt=""
+                className="xl:w-[10rem] w-[5rem] md:w-[9rem] xl:h-[10rem] md:h-[9rem] h-[5rem] object-cover rounded-full border border-[#2A3C46] border-opacity-[80%]"
+              />
+            )}
           </div>
           <div className="flex gap-[1.5rem] md:gap-[3rem] items-center flex-wrap md:flex-nowrap">
             <div className="flex gap-[1rem] items-center">
@@ -83,20 +130,17 @@ const SpaceDetail = () => {
               </div>
             </div>
             <div>
-              {!isAuthenticated && (
+              {loading && (
+                <div className="">
+                  <Loading />
+                </div>
+              )}
+              {!loading && (
                 <button
                   onClick={joinSpaceHandler}
                   className="whitespace-nowrap py-[0.5rem] px-[1rem] font-Poppins text-[#060B12] text-[1rem] font-normal rounded-md bg-[#79C4EC]"
                 >
                   Join space
-                </button>
-              )}
-              {isAuthenticated && (
-                <button
-                  // onClick={joinSpaceHandler}
-                  className="whitespace-nowrap py-[0.5rem] px-[1rem] font-Poppins text-[#060B12] text-[1rem] font-normal rounded-md bg-[#79C4EC]"
-                >
-                  Joined
                 </button>
               )}
             </div>
@@ -106,7 +150,7 @@ const SpaceDetail = () => {
         <div className="font-Poppins mt-[1rem] flex flex-col gap-[0.6rem]">
           <div className="flex gap-[0.2rem] md:gap-[2rem] items-center flex-wrap md:flex-nowrap">
             <p className="text-[#FFF] text-[1.5rem] md:text-[2rem] font-normal normal-case font-Poppins">
-              Audaxious Inc
+              {spaceDetail.title}
             </p>
             <p className="text-[#6DE6AE] text-[0.7rem] md:text-[1rem] font-normal font-Poppins">
               20 Active campaigns
@@ -114,13 +158,25 @@ const SpaceDetail = () => {
           </div>
           <div className="w-[100%] md:w-[90%] max-w-[35rem]">
             <p className="text-[#A5A5A5] text-[0.875rem] md:text-[1.25rem] leading-[140%] font-Poppins">
-              The next-gen smart platform for multi-chain yield maximization and
-              with deflationary ABRA token. Boost yields, automate manual
-              actions, save gas and your time.
+              {spaceDetail.description}
             </p>
           </div>
           <div className="flex gap-[1rem] flex-wrap">
-            <div className="cursor-pointer px-[1rem] py-[0.5rem] flex items-center gap-[0.5rem] border-[#19242D] border-[2px] rounded-[40px]">
+            {spaceDetail.tags &&
+              spaceDetail.tags.map((tag, index) => (
+                <div
+                  key={index}
+                  className="cursor-pointer px-[1rem] py-[0.5rem] flex items-center gap-[0.5rem] border-[#19242D] border-[2px] rounded-[40px]"
+                >
+                  <span>
+                    <Defi />
+                  </span>
+                  <span className="whitespace-nowrap font-Poppins text-[0.87rem] text-[#D3D3D3] font-normal">
+                    {tag}
+                  </span>
+                </div>
+              ))}
+            {/* <div className="cursor-pointer px-[1rem] py-[0.5rem] flex items-center gap-[0.5rem] border-[#19242D] border-[2px] rounded-[40px]">
               <span>
                 <Defi />
               </span>
@@ -151,7 +207,7 @@ const SpaceDetail = () => {
               <span className="font-Poppins text-[0.87rem] text-[#D3D3D3] font-normal">
                 Music
               </span>
-            </div>
+            </div> */}
           </div>
         </div>
 
