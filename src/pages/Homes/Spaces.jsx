1409b19007ed9e8f6472c06706996bbe33efdff4
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-21 13:42:06 +0100

    getting spaces from database

--- src/pages/Homes/Spaces.jsx
+++ src/pages/Homes/Spaces.jsx
@@ -7,11 +7,13 @@ import { ReactComponent as Discords } from "../../assets/svg/dashboardSvg/discor
 import { useEffect, useRef, useState } from "react";
 import { animate, motion, useAnimation, useMotionValue } from "framer-motion";
 import useMeasure from "react-use-measure";
+import { useSelector } from "react-redux";
 
 const Spaces = () => {
   const [ref, { width }] = useMeasure();
   const xTranslation = useMotionValue(0);
   const controls = useAnimation();
+  const spaceArray = useSelector((state) => state.space.space);
 
   useEffect(() => {
     const finalPosition = -width / 2 - 8;
@@ -59,22 +61,29 @@ const Spaces = () => {
           onHoverStart={handleHoverStart}
           onHoverEnd={handleHoverEnd}
         >
-          {SPACES &&
-            [...SPACES, ...SPACES].map((space, index) => (
-              <NavLink to={`/spaces/${space.id}`} key={index}>
+          {spaceArray &&
+            [...spaceArray, ...spaceArray].map((space, index) => (
+              <NavLink to={`/spaces/${space.uuid}`} key={index}>
                 <div className="px-[0.5rem] md:px-[1.5rem] pt-[0.75rem] w-[20rem] hover:border-[3px] pb-[0.75rem] border-[#2A3C46] border border-opacity-[80%] bg-ElipseBg bg-no-repeat bg-cover rounded-[16px] cursor-pointer">
                   <div className="flex flex-col gap-[0.75rem]">
                     <div className="flex items-center justify-between">
                       <div className="flex items-center gap-[0.5rem]">
-                        <img
-                          src={space.src}
-                          alt={space.userName.slice(0, 7)}
-                          width="100"
-                          height={"100"}
-                          className="w-[4rem] h-[4rem] object-cover rounded-full"
-                        />
+                        {!space.src && (
+                          <div className="w-[3rem] h-[3rem] rounded-full bg-slate-200 flex items-center justify-center text-[2rem] text-[#2A3C46]">
+                            {space.title.slice(0, 1)}
+                          </div>
+                        )}
+                        {space.src && (
+                          <img
+                            src={space.src}
+                            alt={space.title.slice(0, 7)}
+                            width="100"
+                            height={"100"}
+                            className="w-[4rem] h-[4rem] object-cover rounded-full"
+                          />
+                        )}
                         <span className="text-[1rem] text-[#FFF] font-[400]">
-                          {space.userName}
+                          {space.title}
                         </span>{" "}
                       </div>
                     </div>
