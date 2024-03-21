84c101e6692f2d795b4481370032ba38f5194557
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-21 13:38:43 +0100

    add campaign

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authenticationSlice from "./authorizationSlice";
 import spaceSlice from "./spaceSlice";
+import campaignSlice from "./campaignSlice";
 
 const store = configureStore({
   reducer: {
     authentication: authenticationSlice.reducer,
     space: spaceSlice.reducer,
+    campaign: campaignSlice.reducer,
   },
 });
 
