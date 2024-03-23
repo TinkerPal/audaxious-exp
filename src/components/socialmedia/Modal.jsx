394cd020bf22d06026d2bea2670c0abf40213b82
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-23 07:07:40 +0100

    modified modal

--- src/components/socialmedia/Modal.jsx
+++ src/components/socialmedia/Modal.jsx
@@ -8,14 +8,14 @@ const Modal = ({ children, onClose, open }) => {
         as="div"
         className={`relative z-[3]`}
         open={open}
-        onClose={() => {}}
+        onClose={onClose}
       >
         <div
           className="fixed inset-0 bg-black bg-opacity-75"
           onClick={onClose}
         />
         <div className="fixed inset-0 z-[4] overflow-y-auto">
-          <div className="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0">
+          <div className="flex items-center justify-center min-h-full p-4 text-center sm:items-center sm:p-0">
             <Dialog.Panel className="">{children}</Dialog.Panel>
           </div>
         </div>
