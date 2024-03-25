f00f4f28a7cebfde3d62dcb30be20dc6c3ce3ff5
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-25 18:00:33 +0100

    create campaign

--- src/pages/newAuth-user/spaces/CreateCampaign.jsx
+++ src/pages/newAuth-user/spaces/CreateCampaign.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import CampaignInformation from "./CampaignInformation";
+import CampaignTasks from "./CampaignTasks";
 
 const CreateCampaign = () => {
+  const [showCampaignTask, setShowCampaignTask] = useState(false);
   return (
     <div>
-      <CampaignInformation />
+      {!showCampaignTask && (
+        <CampaignInformation setShowCampaignTask={setShowCampaignTask} />
+      )}
+      {showCampaignTask && <CampaignTasks />}
     </div>
   );
 };
