3c39d194d845b1ac6d14e62411e61ebeb82e8bd3
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-26 16:05:18 +0100

    custom hook input for number

--- src/hooks/useInput.jsx
+++ src/hooks/useInput.jsx
@@ -32,6 +32,36 @@ const useInput = (valid) => {
 
 export default useInput;
 
+export const useInputNumber = (valid) => {
+  const [value, setValue] = useState(0);
+  const [isTouch, setIsTouch] = useState(null);
+
+  const valueIsValid = valid(value);
+  const valueIsInvalid = !valueIsValid && isTouch;
+
+  const onChangeValueHandler = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onBlurHandler = () => {
+    setIsTouch(true);
+  };
+
+  const reset = () => {
+    setValue(null);
+    setIsTouch(false);
+  };
+
+  return {
+    value,
+    onChangeValueHandler,
+    valueIsValid,
+    valueIsInvalid,
+    onBlurHandler,
+    reset,
+  };
+};
+
 export const useImage = () => {
   const [image, setImage] = useState("");
 
