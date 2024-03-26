a1d2ef4a0a6f3126294c791521c4e0470049074e
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-26 16:09:41 +0100

    src/pages/newAuth-user/spaces/CampaignTasks.jsx

--- src/pages/newAuth-user/spaces/CampaignTasks.jsx
+++ src/pages/newAuth-user/spaces/CampaignTasks.jsx
@@ -6,8 +6,14 @@ import { ReactComponent as Checked } from "../../../assets/svg/dashboardSvg/mark
 import Button from "../../../widget/Button";
 import { useState } from "react";
 import { toast } from "react-toastify";
+import clsx from "clsx";
+import { useInputNumber } from "../../../hooks/useInput";
 
-const CampaignTasks = () => {
+const checkPointValidity = (point) => {
+  return point > 0;
+};
+
+const CampaignTasks = ({ spaceDetail }) => {
   const TASKSBAR = [
     { actions: "Follow Space", image: "/tweetImages/audaxious.svg" },
     { actions: "Follow twitter", image: "/tweetImages/tweeter.svg" },
@@ -21,8 +27,20 @@ const CampaignTasks = () => {
   const [url, setUrl] = useState("");
   const [isTaskAdded, setIsTaskAdded] = useState([]);
 
-  // console.log(selectedTask);
-  console.log(tasks);
+  const {
+    onChangeValueHandler: pointOnchange,
+    reset: resetPoint,
+    value: point,
+    onBlurHandler: pointOnBlur,
+    valueIsInvalid: pointInvalid,
+  } = useInputNumber(checkPointValidity);
+
+  const taskss = {
+    tasks: tasks,
+  };
+
+  console.log(point);
+  console.log(taskss);
 
   // console.log(url);
 
@@ -34,6 +52,14 @@ const CampaignTasks = () => {
     const updatedTasks = [...tasks];
     updatedTasks.splice(index, 1);
     setTasks(updatedTasks);
+    const updatedSelectedTasks = [...selectedTask];
+    updatedSelectedTasks.splice(index, 1);
+    setSelectedTask(updatedSelectedTasks);
+    setIsTaskAdded((prev) => {
+      const updatedIsTaskAdded = [...prev];
+      updatedIsTaskAdded.splice(index, 1);
+      return updatedIsTaskAdded;
+    });
   };
 
   const addTaskClickHandler = (task, url, index) => {
@@ -58,7 +84,26 @@ const CampaignTasks = () => {
 
   // console.log(selectedTask);
   const handleTaskClick = (task) => {
+    // Check if the task is "Follow Space"
+    if (task.actions === "Follow Space") {
+      const taskExists = selectedTask.some((t) => t.actions === task.actions);
+      if (taskExists) {
+        toast.error("Follow Space task already selected");
+        return;
+      }
+    }
+
+    // If the task is not "Follow Space" or it's the first time "Follow Space" is being added, proceed to add it
     setSelectedTask((prev) => [...prev, task]);
+  };
+  const submitTaskHandler = (e) => {
+    e.preventDefault();
+    const data = {
+      tasks,
+      point: +point,
+    };
+
+    console.log(data);
   };
   return (
     <div className="bg-[#060B12] relative py-[2rem] rounded-md w-[100%] min-w-[15rem] lg:w-[50rem] xl:w-[50rem] 2xl:w-[65rem] px-[1rem] lg:px-[3rem] text-[#E8E8E8]r">
@@ -71,7 +116,11 @@ const CampaignTasks = () => {
             Campaign tasks
           </p>
         </div>
-        <div className="flex flex-col gap-[1.88rem] px-[1rem] md:px-[3rem] 2xl:px-[4.5rem]">
+
+        <form
+          onSubmit={submitTaskHandler}
+          className="flex flex-col gap-[1.88rem] px-[1rem] md:px-[3rem] 2xl:px-[4.5rem]"
+        >
           <div className="font-Poppins text-[#E8E8E8] text-[0.75rem] font-[275] flex gap-[0.4rem]">
             <p>Select task type</p>
             <span>
@@ -125,57 +174,171 @@ const CampaignTasks = () => {
               </div>
             )}
             {selectedTask.map((task, index) => {
-              console.log(task);
-              return (
-                <div className="flex gap-2 md:gap-5 my-5" key={index}>
-                  <div
-                    onClick={() =>
-                      addTaskClickHandler(task.actions, url, index)
-                    }
-                    className="cursor-pointer py-[0.56rem] w-[15rem] px-[1rem] rounded-md border-[1px] border-[#295D7C4D] border-opacity-[30%] flex items-center justify-between"
-                  >
-                    <span>
-                      <img
-                        src={task.image}
-                        alt=""
-                        className="w-[1rem] h-[1rem]"
-                      />
-                    </span>
-                    <p className="text-[0.7rem] md:text-[0.9rem] font-Poppins font-[300] text-[#D3D3D3] whitespace-nowrap">
-                      {task.actions}
-                    </p>
-                    {!isTaskAdded[index] ? (
-                      <span>
-                        <AddIcon />
-                      </span>
-                    ) : (
-                      <span>
-                        <Checked />
-                      </span>
-                    )}
-                  </div>
-                  <div className="relative w-[100%]">
-                    <input
-                      required
-                      type="text"
-                      name="name"
-                      id="name"
-                      onChange={(e) => handleInputChange(index, e)}
-                      //   value={title}
-                      //   onChange={titleOnchange}
-                      //   onBlur={titleOnBlur}
-                      placeholder="Enter your campaign title"
-                      className="bg-transparent text-[#FFF] outline-none md:pr-[3rem] placeholder:text-[#A5A5A5] w-[100%] font-[275] bg-[#081017] border-[#436C82] border-[0.75px] border-opacity-[80%] rounded-md px-[1rem] py-[0.62rem] text-[0.75rem] font-Poppins"
-                    />
-                    <span
-                      onClick={() => removeTaskClickHandler(index)}
-                      className="absolute right-3 top-3 md:top-3 text-[0.7rem] md:text-[0.875rem] font-Poppins font-[300] text-[#79C4EC]"
+              // console.log(task);
+              // return (
+              //   <div className="flex gap-2 md:gap-5 my-5" key={index}>
+              //     <div
+              //       onClick={() =>
+              //         addTaskClickHandler(task.actions, url, index)
+              //       }
+              //       className="cursor-pointer py-[0.56rem] w-[15rem] px-[1rem] rounded-md border-[1px] border-[#295D7C4D] border-opacity-[30%] flex items-center justify-between"
+              //     >
+              //       <span>
+              //         <img
+              //           src={task.image}
+              //           alt=""
+              //           className="w-[1rem] h-[1rem]"
+              //         />
+              //       </span>
+              //       <p className="text-[0.7rem] md:text-[0.9rem] font-Poppins font-[300] text-[#D3D3D3] whitespace-nowrap">
+              //         {task.actions}
+              //       </p>
+              //       {!isTaskAdded[index] ? (
+              //         <span>
+              //           <AddIcon />
+              //         </span>
+              //       ) : (
+              //         <span>
+              //           <Checked />
+              //         </span>
+              //       )}
+              //     </div>
+              //     <div className="relative w-[100%]">
+              //       <input
+              //         required
+              //         type="text"
+              //         name="name"
+              //         id="name"
+              //         onChange={(e) => handleInputChange(index, e)}
+              //         //   value={title}
+              //         //   onChange={titleOnchange}
+              //         //   onBlur={titleOnBlur}
+              //         placeholder="Enter your campaign title"
+              //         className="bg-transparent text-[#FFF] outline-none md:pr-[3rem] placeholder:text-[#A5A5A5] w-[100%] font-[275] bg-[#081017] border-[#436C82] border-[0.75px] border-opacity-[80%] rounded-md px-[1rem] py-[0.62rem] text-[0.75rem] font-Poppins"
+              //       />
+              //       <span
+              //         onClick={() => removeTaskClickHandler(index)}
+              //         className="absolute right-3 top-3 md:top-3 text-[0.7rem] md:text-[0.875rem] font-Poppins font-[300] text-[#79C4EC]"
+              //       >
+              //         <Cancel />
+              //       </span>
+              //     </div>
+              //   </div>
+              // );
+              if (task.actions === "Follow Space") {
+                return (
+                  <div className="flex gap-2 md:gap-5 my-5" key={index}>
+                    <div
+                      onClick={() =>
+                        addTaskClickHandler(
+                          task.actions,
+                          `${spaceDetail.uuid}`,
+                          index
+                        )
+                      }
+                      className={clsx(
+                        "cursor-pointer py-[0.56rem] w-[15rem] px-[1rem] rounded-md flex items-center justify-between",
+                        isTaskAdded[index]
+                          ? "border-[2px] border-[#D1E8F4] border-opacity-30 bg-[#09141A]"
+                          : "border-[1px] border-[#295D7C4D] border-opacity-[30%]"
+                      )}
                     >
-                      <Cancel />
-                    </span>
+                      <span>
+                        <img
+                          src={task.image}
+                          alt=""
+                          className="w-[1rem] h-[1rem]"
+                        />
+                      </span>
+                      <p className="text-[0.7rem] md:text-[0.9rem] font-Poppins font-[300] text-[#D3D3D3] whitespace-nowrap">
+                        {task.actions}
+                      </p>
+                      {!isTaskAdded[index] ? (
+                        <span>
+                          <AddIcon />
+                        </span>
+                      ) : (
+                        <span>
+                          <Checked />
+                        </span>
+                      )}
+                    </div>
+                    <div className="relative w-[100%]">
+                      <input
+                        required
+                        type="text"
+                        name="name"
+                        id="name"
+                        defaultValue={`${spaceDetail.uuid}`}
+                        disabled // Disable the input field
+                        className="cursor-not-allowed disabled:bg-[#0A1017] bg-transparent text-[#FFF] outline-none md:pr-[3rem] placeholder:text-[#A5A5A5] w-[100%] font-[275] bg-[#081017] border-[#436C82] border-[0.75px] border-opacity-[80%] rounded-md px-[1rem] py-[0.62rem] text-[0.75rem] font-Poppins"
+                      />
+                      <span
+                        onClick={() => removeTaskClickHandler(index)}
+                        className="absolute right-3 top-3 md:top-3 text-[0.7rem] md:text-[0.875rem] font-Poppins font-[300] text-[#79C4EC]"
+                      >
+                        <Cancel />
+                      </span>
+                    </div>
                   </div>
-                </div>
-              );
+                );
+              } else {
+                return (
+                  <div className="flex gap-2 md:gap-5 my-5" key={index}>
+                    <div
+                      onClick={() =>
+                        addTaskClickHandler(task.actions, url, index)
+                      }
+                      className={clsx(
+                        "cursor-pointer py-[0.56rem] w-[15rem] px-[1rem] rounded-md flex items-center justify-between",
+                        isTaskAdded[index]
+                          ? "border-[2px] border-[#D1E8F4] border-opacity-30 bg-[#09141A]"
+                          : "border-[1px] border-[#295D7C4D] border-opacity-[30%]"
+                      )}
+                    >
+                      <span>
+                        <img
+                          src={task.image}
+                          alt=""
+                          className="w-[1rem] h-[1rem]"
+                        />
+                      </span>
+                      <p className="text-[0.7rem] md:text-[0.9rem] font-Poppins font-[300] text-[#D3D3D3] whitespace-nowrap">
+                        {task.actions}
+                      </p>
+                      {!isTaskAdded[index] ? (
+                        <span>
+                          <AddIcon />
+                        </span>
+                      ) : (
+                        <span>
+                          <Checked />
+                        </span>
+                      )}
+                    </div>
+                    <div className="relative w-[100%]">
+                      <input
+                        required
+                        type="text"
+                        name="name"
+                        id="name"
+                        onChange={(e) => handleInputChange(index, e)}
+                        //   value={title}
+                        //   onChange={titleOnchange}
+                        //   onBlur={titleOnBlur}
+                        placeholder="Enter your campaign title"
+                        className="bg-[#0A1017] text-[#FFF] outline-none md:pr-[3rem] placeholder:text-[#A5A5A5] w-[100%] font-[275] border-[#436C82] border-[0.75px] border-opacity-[80%] rounded-md px-[1rem] py-[0.62rem] text-[0.75rem] font-Poppins"
+                      />
+                      <span
+                        onClick={() => removeTaskClickHandler(index)}
+                        className="absolute right-3 top-3 md:top-3 text-[0.7rem] md:text-[0.875rem] font-Poppins font-[300] text-[#79C4EC]"
+                      >
+                        <Cancel />
+                      </span>
+                    </div>
+                  </div>
+                );
+              }
             })}
           </div>
           <div className="flex flex-col md:flex-row">
@@ -192,22 +355,27 @@ const CampaignTasks = () => {
 
               <input
                 required
-                type="text"
+                type="number"
                 name="point"
                 id="point"
-                //   value={title}
-                //   onChange={titleOnchange}
-                //   onBlur={titleOnBlur}
+                value={point}
+                onChange={pointOnchange}
+                onBlur={pointOnBlur}
                 placeholder="Enter your campaign title"
                 className="text-[#FFF] px-[1rem] outline-none placeholder:text-[#A5A5A5] font-[275] bg-[#09141A] border-[#F0894F] border-[0.75px] rounded-md py-[0.62rem] text-[0.75rem] font-Poppins"
               />
+              {pointInvalid && (
+                <p className="text-[#b40e0e] text-end text-[0.75rem] font-[600] font-Poppins">
+                  {"Enter valid point format"}
+                </p>
+              )}
             </div>
             <div className="flex gap-5 items-center text-[#FFF] mt-[2rem] md:mt-[1.5rem]">
-              <Button>Save Draft</Button>
-              <Button>Lunch</Button>
+              <Button type={"button"}>Save Draft</Button>
+              <Button type={"submit"}>Lunch</Button>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
