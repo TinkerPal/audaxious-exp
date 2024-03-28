a77354b114738d7b6b501ec81f2195f5593b2b63
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-03-28 20:17:59 +0100

    added an array to completed task

--- src/store/campaignActions.js
+++ src/store/campaignActions.js
@@ -1,4 +1,7 @@
-import { createAxiosPublicInstance } from "../hooks/authInstance";
+import {
+  createAxiosPublicInstance,
+  createAxiousInstance,
+} from "../hooks/authInstance";
 
 export const getAllCampaigns = () => {
   return async () => {
@@ -40,6 +43,26 @@ export const getAllCampaignsBySpace = (spaceId) => {
     }
   };
 };
+export const getAllCompletedTask = (id) => {
+  return async () => {
+    const getCompletedTask = async () => {
+      const response = createAxiosPublicInstance();
+      try {
+        const responseData = await response.get(`task/completed/${id}`);
+        return responseData.data;
+      } catch (error) {
+        console.log("throw Error", error);
+        throw error;
+      }
+    };
+    try {
+      return await getCompletedTask();
+    } catch (error) {
+      console.log("throw Error", error);
+      throw error;
+    }
+  };
+};
 
 export const getCampaignById = (campaignId) => {
   return async () => {
@@ -57,6 +80,53 @@ export const getCampaignById = (campaignId) => {
       return await getSingleCampaign();
     } catch (error) {
       console.log("throw Error", error);
+      throw error;
+    }
+  };
+};
+
+export const participateInTask = (id) => {
+  return async () => {
+    const token = localStorage.getItem("audaxiousAccessToken") || null;
+    const postAllTask = async () => {
+      const request = createAxiousInstance(token);
+      try {
+        const response = await request.post(`/task/participate/all/${id}`);
+        return response.data;
+      } catch (error) {
+        console.log("INITIAL PARTICIPATE IN TASK ERROR");
+        throw error;
+      }
+    };
+
+    try {
+      const data = await postAllTask();
+      return data;
+    } catch (error) {
+      console.log("HANDLE PARTICIPATE IN TASK ERROR", error);
+      throw error;
+    }
+  };
+};
+export const ClaimPointTask = (id) => {
+  return async () => {
+    const token = localStorage.getItem("audaxiousAccessToken") || null;
+    const claimTask = async () => {
+      const request = createAxiousInstance(token);
+      try {
+        const response = await request.post(`/points/claim/${id}`);
+        return response.data;
+      } catch (error) {
+        console.log("INITIAL CLAIM POINT ERROR");
+        throw error;
+      }
+    };
+
+    try {
+      const data = await claimTask();
+      return data;
+    } catch (error) {
+      console.log("HANDLE CLAIM POINT ERROR", error);
       throw error;
     }
   };
