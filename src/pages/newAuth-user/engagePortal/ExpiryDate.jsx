019e3456d2838d7ba2077c7b034201876f350fe0
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-04-01 23:45:35 +0100

    Expiry date

--- src/pages/newAuth-user/engagePortal/ExpiryDate.jsx
+++ src/pages/newAuth-user/engagePortal/ExpiryDate.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import dayjs from "dayjs";
 
 const ExpiryDate = ({ expiryDate }) => {
@@ -25,7 +25,7 @@ const ExpiryDate = ({ expiryDate }) => {
     calculateTimeLeft();
     const timer = setInterval(() => {
       calculateTimeLeft();
-    }, 1000 * 60);
+    }, 1000 * 60); // Update every minute
 
     return () => clearInterval(timer);
   }, [expiryDate]);
