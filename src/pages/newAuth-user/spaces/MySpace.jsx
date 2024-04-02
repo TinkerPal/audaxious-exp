e427a1b1c0cb9bbce05d9d02c4ccef05e3c9ebbb
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-04-02 17:35:49 +0100

    add additional check for authentication and if array is empty

--- src/pages/newAuth-user/spaces/MySpace.jsx
+++ src/pages/newAuth-user/spaces/MySpace.jsx
@@ -1,36 +1,61 @@
 import React from "react";
-import { SPACES } from "../../../utils/postApi";
-import { ReactComponent as Group } from "../../../assets/svg/dashboardSvg/group.svg";
-import { ReactComponent as World } from "../../../assets/svg/dashboardSvg/world.svg";
-import { ReactComponent as Retweets } from "../../../assets/svg/dashboardSvg/retweets.svg";
-import { ReactComponent as Discords } from "../../../assets/svg/dashboardSvg/discords.svg";
-import Query from "./Query";
-import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { authAction } from "../../../store/authorizationSlice";
 import Loading from "../../Homes/Loading";
-import { joinSpace } from "../../../store/spaceActions";
-import { toast } from "react-toastify";
 import SpaceCard from "./SpaceCard";
+import { ReactComponent as NoSpace } from "../../../assets/svg/dashboardSvg/noSpace.svg";
 
 const MySpace = ({ mySpaces }) => {
   const loading = useSelector((state) => state.space.loading);
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(
+    (state) => state.authentication.isLogedIn
+  );
 
-  // console.log("MYSPACEUUID", mySpaces);
+  const openLoginModal = () => {
+    dispatch(authAction.onOpen());
+  };
 
-  // const joinSpaceHandler = async (id) => {
-  //   if (!isAuthenticated) {
-  //     dispatch(authAction.onOpen());
-  //     return;
-  //   }
-  //   try {
-  //     const result = await dispatch(joinSpace(id));
-  //     toast.success(result.message);
-  //   } catch (error) {
-  //     toast.error(error.response.data.error);
-  //   }
-  // };
+  if (!isAuthenticated) {
+    return (
+      <div className="flex flex-col justify-center mt-[5rem] items-center gap-[1rem]">
+        <div className="font-Poppins gap-[0.5rem] text-[#707171] flex flex-col items-center">
+          <p className="font-[400] text-[1.2rem]">Please login</p>
+          <p className="text-center font-[300] text-[0.8rem] leading-normal">
+            Spaces you created will appear here
+            <br /> login to see <span className="text-[#FFF]">“My Spaces”</span>
+          </p>
+        </div>
+        <button
+          onClick={openLoginModal}
+          className="border-[#2A3C46] border border-opacity-[80%] py-[0.4rem] px-[1rem] rounded-md font-Poppins text-[#E8E8E8] text-[0.75rem] font-[300]"
+        >
+          Login
+        </button>
+      </div>
+    );
+  }
 
+  if (!mySpaces || mySpaces.length <= 0) {
+    return (
+      <div className="flex flex-col justify-center mt-[5rem] items-center gap-[1rem]">
+        <span>
+          <NoSpace />
+        </span>
+        <div className="font-Poppins gap-[0.5rem] text-[#707171] flex flex-col items-center">
+          <p className="font-[400] text-[1.2rem]">No spaces to show</p>
+          <p className="text-center font-[300] text-[0.8rem] leading-normal">
+            Spaces created will appear here
+            <br /> click <span className="text-[#FFF]">“Create space”</span> to
+            to create one
+          </p>
+        </div>
+        <button className="border-[#2A3C46] border border-opacity-[80%] py-[0.4rem] px-[1rem] rounded-md font-Poppins text-[#E8E8E8] text-[0.75rem] font-[300]">
+          Create space
+        </button>
+      </div>
+    );
+  }
   return (
     <div>
       <div className="md:container">
