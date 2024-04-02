a443f02d3e1df40c27113dcec30e72e34a23534f
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-04-02 17:23:38 +0100

    add error slice reducer

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authenticationSlice from "./authorizationSlice";
 import spaceSlice from "./spaceSlice";
 import campaignSlice from "./campaignSlice";
+import errorSlice from "./errorSlice";
 
 const store = configureStore({
   reducer: {
     authentication: authenticationSlice.reducer,
     space: spaceSlice.reducer,
     campaign: campaignSlice.reducer,
+    errors: errorSlice.reducer,
   },
 });
 
