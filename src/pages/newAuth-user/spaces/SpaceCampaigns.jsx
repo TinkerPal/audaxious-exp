855f72f11847ee3a3317dd47d98042b0a2436acc
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-04-04 19:47:37 +0100

    handle condition when there is no space campaigns

--- src/pages/newAuth-user/spaces/SpaceCampaigns.jsx
+++ src/pages/newAuth-user/spaces/SpaceCampaigns.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { ReactComponent as NoSpace } from "../../../assets/svg/dashboardSvg/noSpace.svg";
 import CampaignsCard from "../engagePortal/CampaignCard";
 
 const SpaceCampaigns = ({ spaceId }) => {
   const campaigns = useSelector((state) => state.space.spaceCampaigns);
+  if (!campaigns || campaigns.length <= 0) {
+    return (
+      <div className="flex flex-col justify-center mt-[5rem] items-center gap-[1rem]">
+        <span>
+          <NoSpace />
+        </span>
+        <div className="font-Poppins gap-[0.5rem] text-[#707171] flex flex-col items-center">
+          <p className="font-[400] text-[1.2rem]">No campaigns to show</p>
+          <p className="text-center font-[300] text-[0.8rem] leading-normal">
+            All Campaigns will appear here
+            <br /> <span className="text-[#FFF]">“Search”</span> for a valid
+            campaign title
+          </p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col items-center">
