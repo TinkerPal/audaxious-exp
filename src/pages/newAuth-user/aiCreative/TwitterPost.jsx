07ced8b3cd6dd22278283736d7ff1727436d61cb
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-04-15 14:07:24 +0100

    mapped saved post

--- src/pages/newAuth-user/aiCreative/TwitterPost.jsx
+++ src/pages/newAuth-user/aiCreative/TwitterPost.jsx
@@ -1,36 +1,12 @@
 import { ReactComponent as NoPost } from "../../../assets/svg/dashboardSvg/noPost.svg";
-import Button from "../../../../src/widget/Button";
-import { useDispatch } from "react-redux";
-import { aiCreatieActions } from "../../../store/aiCreativeSlice";
-import { TWITTERPOST } from "../../../utils/postApi";
-import TwitterPostCard from "./TwitterPostCard";
+import { useSelector } from "react-redux";
 import Pagination from "../../../widget/Pagination";
 import AiPost from "./AiPost";
 
 import SearchAI from "./SearchAI";
 const TwitterPost = () => {
-  const dispatch = useDispatch();
-  const openModal = () => {
-    dispatch(aiCreatieActions.setCreatePostModal(true));
-  };
-  if (TWITTERPOST.length <= 0) {
-    return (
-      <div className="flex flex-col justify-center mt-[5rem] items-center gap-[1rem] text-[#707171]   ">
-        <span>
-          <NoPost />
-        </span>
-        <div className="font-Poppins gap-[0.5rem] text-[#707171] flex flex-col items-center">
-          <h3 className="font-[400] text-[1.2rem]">Nothing to see here!</h3>
-          <p className="text-center font-[300] text-[0.8rem] leading-normal">
-            Your post will appear here{" "}
-          </p>
-        </div>
-        <Button onClick={openModal} type={"button"}>
-          Create post
-        </Button>
-      </div>
-    );
-  }
+  const savedPost = useSelector((state) => state.aiCreative.savedPost);
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 items-start pb-5 ">
       <AiPost />
@@ -42,12 +18,32 @@ const TwitterPost = () => {
           TWITTERPOST.filter((post, index) => index < 5).map((post, index) => {
             return <TwitterPostCard post={post} key={index} />;
           })} */}
-          <div className="flex flex-col  justify-between border-white/20 border-[#07111c] bg-[#060e16] hover:border-white/70 p-4 rounded-md   hover:border-[#14435c]  text-white/70 border border-opacity-[80%]   cursor-pointer  ">
-            🚀 AudaXious Engage-to-Earn platform is going live and we will be
-            rewarding early members and adopters of our platform with ADX token.
-            To participate, please complete the listed tasks.
-          </div>
+          {savedPost.length <= 0 && (
+            <div className="flex flex-col justify-center mt-[5rem] items-center gap-[1rem] text-[#707171]   ">
+              <span>
+                <NoPost />
+              </span>
+              <div className="font-Poppins gap-[0.5rem] text-[#707171] flex flex-col items-center">
+                <h3 className="font-[400] text-[1.2rem]">
+                  Your saved post will appear here{" "}
+                </h3>
+              </div>
+              {/* <Button onClick={openModal} type={"button"}>
+                Create post
+              </Button> */}
+            </div>
+          )}
+          {savedPost.length > 0 &&
+            savedPost.map((post, index) => (
+              <div
+                key={index}
+                className="flex flex-col  justify-between border-white/20 border-[#07111c] bg-[#060e16] hover:border-white/70 p-4 rounded-md   hover:border-[#14435c]  text-white/70 border border-opacity-[80%] cursor-pointer w-[100%]"
+              >
+                {post}
+              </div>
+            ))}
 
+          {/* 
           <div className="flex flex-col  justify-between border-white/20 border-[#07111c] bg-[#060e16] hover:border-white/70 p-4 rounded-md   hover:border-[#14435c]  border border-opacity-[80%]  text-white/70  cursor-pointer ">
             🚀 AudaXious Engage-to-Earn platform is going live and we will be
             rewarding early members and adopters of our platform with ADX token.
@@ -70,7 +66,7 @@ const TwitterPost = () => {
             rewarding early members and adopters of our platform with ADX token.
             To participate, please complete the listed tasks. AudaXious
             Engage-to-Earn platform is going live and we will be rewarding early
-          </div>
+          </div> */}
         </div>
         <Pagination />
       </div>
